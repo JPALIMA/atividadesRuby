c8747736bc3d53db9a8e734ab6187187b2a12174
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-05-02 21:50:21 -0300

    modificação de codigos na pasta the_project_ruby

--- the_odin_project_ruby/numeros.rb
+++ the_odin_project_ruby/numeros.rb
@@ -26,6 +26,8 @@
 
 10 % 4 #=> 2 (10 / 4 = 2 with a remainder of 2)
 
+#inteiros e Flutuantes
+
 17/5 #=> 3, not 3.4
 
 17/5.0 #=> 3.4
--- the_odin_project_ruby/string.rb
+++ the_odin_project_ruby/string.rb
@@ -4,6 +4,9 @@
 
 "welcome " + "to " + "Study!" #=> "welcome to Odin!"
 
+# with the shovel operator:
+"welcome " << "to " << "Odin!" #=> "Welcome to Odin!"
+
 # With the concat method:
 
 #"welcome".concat ("to ").concat("Study!") #=> "Welcome to Study"
@@ -14,3 +17,20 @@
 "hello"[0..1] #=> "he"
 "hello"´[0,4] #=> "hell"
 "hello"[-1] #=> "o"
+
+#Escape Characters
+
+\\ #=> Need a backslash in you string
+\b #=> Backspace
+\r #=> Carriage return, for those of you that love tipewriters
+\n #=> Newline. you'll likely use this one the most.
+\s #=> Space
+\t #=> Tab
+\" #=> Double quotation mark
+\' #=> Single quotation mark
+
+name = "odin"
+
+puts "Hello, #{name}"  #=> "hello, Odin"
+puts 'hello, #{name}' #=> "Hello, #{name}"
+
