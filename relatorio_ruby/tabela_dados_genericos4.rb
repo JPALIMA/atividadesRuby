8a5ec367917ff443333cc8683e35998f7c43cd7f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-14 14:23:00 -0300

    renomeando o projeto 'total_vendas_por_mes4.rb' para 'tabela_dados_genericos4.rb'.

--- relatorio_ruby/tabela_dados_genericos4.rb
+++ relatorio_ruby/tabela_dados_genericos4.rb
@@ -2,23 +2,23 @@ require 'prawn'
 
 Prawn::Fonts::AFM.hide_m17n_warning = true
 
-data = [
-  ['ID', 'Nome', 'Idade'],
-  [1, 'João', 30],
-  [2, 'Maria', 28],
-  [3, 'Pedro', 35]
-]
-
 Prawn::Document.generate('relatorio.pdf') do
-  text 'Relatório de Dados', align: :center, size: 18, style: :bold
+  image 'C:\\Users\\JOAO PEDRO\\Documents\\Atividades_basicas_ruby\\relatorio_ruby\\logo.png', width: 150,
+  position: :center
   move_down 20
 
-  data.each do |row|
-    row.each_with_index do |cell, index|
-      bounding_box([index * 200, cursor], width: 200) do
-        text cell.to_s
-      end
-    end
-    move_down 20
+  text 'Relatório dos Produtos', align: :center, size: 18, style: :bold
+  move_down 20
+
+  text 'A seguir, apresentamos alguns dos produtos mais vendidos:', style: :italic
+
+  products = [
+    { name: 'Produto A', price: '$10.00'},
+    { name: 'Produto B', price: '$15.00'},
+    { name: 'Produto C', price: '$20.00'}
+  ]
+
+  products.each do |product|
+    text "#{product[:name]}: #{product[:price]}", size: 14, indent_paragraphs: 20
   end
 end
