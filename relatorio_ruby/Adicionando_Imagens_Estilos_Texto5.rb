505735b317c8113f4d5e090ad6f3eb351ea5ef9f
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-15 11:16:20 -0300

    codigo aplicando um script em Ruby usando a biblioteca Prawn para gerar um relatório em formato PDF.

--- relatorio_ruby/Adicionando_Imagens_Estilos_Texto5.rb
+++ relatorio_ruby/Adicionando_Imagens_Estilos_Texto5.rb
@@ -1,23 +1,22 @@
 require 'prawn'
 
-Prawn::Document.generate('relatorio.pdf') do 
-     image 'C:\\Users\\JOAO PEDRO\\Documents\\Atividades_basicas_ruby\\relatorio_ruby\\logo.png',width: 150,
+Praen::Document.generate('relatorio.pdf') do 
+     image 'C:\\Users\\JOAO PEDRO\\Documents\\Atividade_basicas_ruby\\relatorio_ruby\\logo.png', width: 150,
      position: :center
      move_down 20
 
-     text 'Relatório de Produtos', align: :center, size: 18, style: :bold
+     text 'Relatorio de Produtos', align: :center, size: 18, style: bold
      move_down 20
 
-     text 'A seguir, apresentamos alguns dos produtos mais vendidos:', style: :italic
+     text 'A seguir, apresentamos alguns produtos mais vendidos:', style: :italic
 
-     products = [
+     produtos = [
           { name: 'Produto A', price: '$10.00'},
           { name: 'Produto B', price: '$15.00'},
           { name: 'Produto C', price: '$20.00'}
      ]
 
-     products.each do |product|
-          text "#{product[:name]}: #{product[:price]}", size: 14, indent_paragraphs: 20
+     produtos.each do |produto|
+          text "#{produto[:name]}: #{produto[:price]}", size: 14, indent_paragraphs: 20
      end
 end
-
