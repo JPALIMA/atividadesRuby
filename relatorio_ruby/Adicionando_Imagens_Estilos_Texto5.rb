88f823cf05f0092566ca235abca2d6ffe54e659d
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-30 21:32:42 -0300

    geração de relatorio simples Adicionando Imagens e Estilos de Texto' utilizando a biblioteca Prawn.

--- relatorio_ruby/Adicionando_Imagens_Estilos_Texto5.rb
+++ relatorio_ruby/Adicionando_Imagens_Estilos_Texto5.rb
@@ -1,21 +1,22 @@
 require 'prawn'
 
-Prawn::Fontes::AFM.hide_m17n_warning = true
+Prawn::Fonts::AFM.hide_m17n_warning = true
 
-prawn::Document.generate('relatorio.pdf') do
+prawn::Document.generate('relatotio.pdf') do
      image 'C:\\User\\JOAO PEDRO\\Documents\\Atividades_basicas_ruby\\relatorio_ruby\\logo.png', widht: 150,
      position: :center
      move_down 20
 
-     text 'Relatorio de Produtos', aligh: :center, size: 18, style: :italic
+     text 'Relatorio de Produtos', aligh: :center, size: 18, style: :intalic
 
      produtos = [
           { name: 'Produto A', price: '$10.00'},
           { name: 'Produto B', price: '$15.00'},
-          { name: 'Produto C', price: '$20.00'} 
+          { name: 'Produto C', price: '$20.00'}
      ]
+end
 
      produtos.each do |produto|
-          text "#{produto[:name]}: #{produto[:price]}", size: 14, indent_paragraphs: 20
+          text "#{produto[:name]}: #{produto[:price]}", size: 14, indent_paragrahs 20
      end
-end
+[end]
