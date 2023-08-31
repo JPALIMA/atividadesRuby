80154fcfb5b60867d15e45108696dd15a5efcc1c
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-08-31 19:41:44 -0300

    geração de relatorio simples Adicionando Imagens e Estilos de Texto' utilizando a biblioteca Prawn.

--- relatorio_ruby/Adicionando_Imagens_Estilos_Texto5.rb
+++ relatorio_ruby/Adicionando_Imagens_Estilos_Texto5.rb
@@ -3,22 +3,21 @@ require 'prawn'
 Prawn::Fonts::AFM.hide_m17n_warning = true
 
 Prawn::Document.generate('relatorio.pdf') do
-  image 'C:\\Users\\JOAO PEDRO\\Documents\\Atividades_basicas_ruby\\relatorio_ruby\\logo.png', width: 150,
-        position: :center
-  move_down 20
+     image 'C:\\Users\\JOAO PEDRO\\Documents\\Atividades_basicas_ruby\\relatorio_ruby\\logo.png', width: 150, position: :center
+     move_down 20
 
-  text 'Relatório de Produtos', align: :center, size: 18, style: :bold
-  move_down 20
+     text 'Relatório de Produtos', align: :center, size: 18, style: :bold
+     move_down 20
 
-  text 'A seguir, apresentamos alguns produtos mais vendidos:', style: :italic
+     text 'A seguir, apresentamos alguns produtos mais vendidos:', style: :italic
 
-  produtos = [
-    { name: 'Produto A', price: '$10.00'},
-    { name: 'Produto B', price: '$15.00'},
-    { name: 'Produto C', price: '$20.00'}
-  ]
+     produtos = [
+          { name: 'Produto A', price: '$10.00'},
+          { name: 'Produto B', price: '$15.00'},
+          { name: 'Produto C', price: '$20.00'}
+     ]
 
-  produtos.each do |produto|
-    text "#{produto[:name]}: #{produto[:price]}", size: 14, indent_paragraphs: 20
-  end
+     produtos.each do |produto|
+          text "#{produto[:name]}: #{produto[:price]}", size: 14, indent_paragraphs: 20
+     end
 end
